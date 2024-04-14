48d758cbac92947499c9bc1616d304002559bf2a
legionJP <jjpsingh18@gmail.com>
2024-04-15 04:21:46 +0530

    updating code for adding the data on web using jinja template

--- app/app.py
+++ app/app.py
@@ -1,29 +1,44 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app= Flask(__name__) #creatimg app variable Flask  and setiing it as a instance of class Flask 
+# app= Flask(__name__) #creatimg app variable Flask  and setiing it as a instance of class Flask 
 
 #__name__ module will be equal to __main__ if be run the our py app directly 
 
 #creating the route , by using app decorator 
 
-
 # @app.route("/") #forward / is route page of our web
+ 
+app = Flask(__name__,template_folder='templates')
 
-from flask import Flask
+#making the list of dict for the post data
 
-app = Flask(__name__)
+posts = [
+    {
+        'author':'AK',
+        'title':'Blog post 2',
+        'content':'more Post Content',
+        'date_posted':'April 15 , 2024'        
+    },
+    { 
+        'author':'JP',
+        'title':'Blog post 1',
+        'content':'First Post Content',
+        'date_posted':'April 15 , 2024'
+    }
+]
+
 
 @app.route('/')
 @app.route('/Home') # adding the multiple route within one fun using the  decorator
 def home():
-    return '<h1>Home Page</h1>'
+    #return '<h1>Home Page</h1>'
+    return render_template('home.html',posts = posts) #passing the var to template so the data of posts will be equal to post data
+
 
 @app.route('/about')
 def about():
-    return '<h1>Hello, this is flask app!</h1>'
-
-
-
+    #return '<h1>Hello, this is flask app!</h1>'
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
