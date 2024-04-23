58c3433036adc496abacdcfe5e682da081d2b28f
legionJP <jjpsingh18@gmail.com>
2024-04-23 12:34:53 +0530

    Adding usrname and email validation

--- flask_app/blog_app/forms.py
+++ flask_app/blog_app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField,BooleanField
-from wtforms.validators import DataRequired , Length, Email, EqualTo
-
+from wtforms.validators import DataRequired , Length, Email, EqualTo , ValidationError
+from blog_app.models import User
 class RegistrationForm(FlaskForm):
     username= StringField('Username',validators=[ DataRequired(),Length(min=2,max=20)])
 
@@ -13,7 +13,20 @@ class RegistrationForm(FlaskForm):
                                     validators= [DataRequired(), EqualTo('password')])
 
     submit =SubmitField('Sign up')
- 
+
+#creating the template for validation 
+
+    def validate_field(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if User:
+            raise ValidationError('This username is taken , please choose another one')
+        
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if User:
+            raise ValidationError('The email is already in use, please choose another one')
+        
+
 class LoginForm(FlaskForm):
     # username= StringField('Username',validators=[data_required(),Length(min=2,max=20)])
     email= StringField('Email', validators=[DataRequired(),Email()])
